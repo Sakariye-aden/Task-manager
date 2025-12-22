f6ce36c5eb8fe05d38fecebd6c4dc090207e5fef
Sakariye-aden <sakariyeadenhaji@gmail.com>
2025-12-22 17:44:53 +0300

    added errorhandler

---
+++ frontend/src/utility/errorHandle.js
@@ -0,0 +1,32 @@
+
+export const errorhandle =(error)=>{
+  
+    if(!error) return null ;
+    
+    if(error.response?.data){
+        const data = error.response.data;
+
+        // handle zod errors 
+       if(data.errors && Array.isArray(data.errors)){
+        return data.errors.map(err=> err.message).join(',')
+       }
+
+     //    handle single error
+      if(data.message){
+        return data.error
+      }
+    }
+
+    // handle error network 
+    if(error.response && !error.response){
+        return 'Network error please check your connection'
+    }
+
+    // fall back 
+    if(error.message){
+        return error.message
+    }
+
+    // handle other types of errors 
+    return 'some thing went wrong please try again '
+}
