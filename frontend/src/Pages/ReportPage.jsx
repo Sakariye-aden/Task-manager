03df3e5de9e404824c941ca3ca82f8b22bb219df
Sakariye-aden <sakariyeadenhaji@gmail.com>
2025-12-31 11:59:08 +0300

    added react chart.js

--- frontend/src/Pages/ReportPage.jsx
+++ frontend/src/Pages/ReportPage.jsx
@@ -1,8 +1,208 @@
+import { useQuery } from "@tanstack/react-query"
+import api from "../lib/Api/ApiClient";
+import { Loader } from "lucide-react";
+import ChartDataLabels from "chartjs-plugin-datalabels";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, CategoryScale, LinearScale, LineElement,
+  PointElement,  Title, Filler} from 'chart.js';
+
+
+import { Line ,  Doughnut  } from "react-chartjs-2";
+
+import useThemeStore from "../lib/Store/ThemeStore";
+
+
+
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, ChartDataLabels, LineElement,
+  PointElement,  Title, Filler);
+
+
+
+
+
 
 const ReportPage = () => {
+
+    const { theme }= useThemeStore()
+ 
+     const {data , error , isLoading }= useQuery({
+       queryKey : ['trans'],
+       queryFn : async () => {
+           const response = await api.get('/transactions');
+            return response.data
+         }
+     })
+
+     if(error){
+        return <h1>Loading......</h1>
+     }
+
+
+     const Expenses = data?.filter((item)=> item.type === 'expense');
+     const Income = data?.filter((item)=> item.type === 'income');
+    //  Total income 
+      const totalExpense = Expenses?.reduce((sum, e) => sum + e.amount, 0);
+      const totalIncome = Income?.reduce((sum, e) => sum + e.amount, 0);
+      const totalBalance = totalIncome - totalExpense
+        
+       console.log('Expex',Expenses);
+
+
+        if(isLoading){
+       return (
+          <div className='h-screen flex justify-center items-center'>
+             <Loader className='animate-spin text-3xl' />
+          </div>
+       )
+     }
+
+      // Doughnut chart info 
+      const Data = {
+        labels: Expenses.map((e) => e.category),
+        datasets: [
+          {
+            data: Expenses.map((e) => e.amount),
+            backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56" , "#FF6384", "#36A2EB", "#FFCE56", "#4BC0C0", "#9966FF"
+
+            ], // customize colors
+            borderRadius: 10, // rounded edges
+            spacing: 2,       // space between segments
+
+          },
+        ],
+      };
+      const options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        cutout: "60%", // controls thickness of doughnut
+        plugins: {
+          legend: {
+            position: "right", // move labels to the left side
+            labels: {
+              usePointStyle: true,
+              boxWidth: 20,
+              pointStyle: "circle",
+              padding: 15,
+              color: `${theme == 'light' ? "#333" : "#fff"}`,
+              font: {
+                size: 14,
+                weight: "bold",
+              },
+            },
+          },
+          datalabels: {
+            color: "#fff",
+            formatter: (value) => {
+              const percentage = ((value / totalExpense ) * 100).toFixed(1) + "%";
+              return percentage;
+            },
+            font: {
+              weight: "bold",
+              size: 12,
+            },
+          },
+        },
+      };
+
+      // Line Chart 
+      // const FakeDays = ["Day-1", "Day-2", "Day-3", "Day-4" , "Day-5","Day-6", "Day-7", "Day-8"];
+      const labelData = data.map((i, idx) => `Day-${idx+1}`);
+
+        const DataLine = {
+          labels: labelData ,
+          datasets: [
+            {
+              label: "Income",
+              data: Income.map((i) => i.amount),
+              borderColor: "green",
+              backgroundColor: "rgba(0, 128, 0, 0.2)", // green background
+              fill: true,
+               tension: 0.4,
+            },
+            {
+              label: "Expense",
+              data: Expenses.map((e) => e.amount),
+              borderColor: "red",
+              backgroundColor: "rgba(255, 0, 0, 0.2)", // red background
+              fill: true,
+               tension: 0.4,
+            },
+          ],
+        };
+
+        const lineOptions = {
+          responsive: true,
+          maintainAspectRatio: false,
+          plugins: {
+            legend: {
+              position: "top",
+              labels: {
+                color: "#333",
+                font: { size: 14, weight: "bold" },
+              },
+            },
+            tooltip: {
+              enabled: true,
+              backgroundColor: "#222",
+              titleColor: "#fff",
+              bodyColor: "#fff",
+            },
+          },
+          scales: {
+            x: {
+              grid: {
+                display: false, // ✅ hides vertical grid lines
+              },
+              ticks: {
+                color: "#666",
+              },
+            },
+            y: {
+              grid: {
+                color: "rgba(200,200,200,0.2)", // subtle horizontal lines
+              },
+              ticks: {
+                color: "#666",
+              },
+            },
+          },
+        };
+
+
+   
+
+
   return (
-    <div>ReportPage</div>
-  )
+    <div className="bg-card h-screen p-6 ">
+      <h1 className="text-2xl font-medium py-2">Reports & insights</h1>
+      <p className="font-medium">
+        Understand your Spending patterns and Financial trends
+      </p>
+      {/* expense category */}
+      <div className="border my-2">
+        <h1 className="text-2xl text-chart-4 font-medium p-2 border-b ">Spending by category</h1>
+        <div className="bg-card p-2 w-full max-w-xl  h-100 m-auto">
+          <Doughnut data={Data} options={options} />
+        </div>
+        <div className="border-t p-2 flex justify-around items-center">
+           <div className="flex flex-col">
+             <span className="text-lg font-bold ">${totalBalance}</span>
+             <span>Balance</span>
+           </div>
+           <div className="flex flex-col">
+             <span className="text-lg font-bold text-red-500">${totalExpense}</span>
+             <span>Expense</span>
+           </div>
+        </div>
+      </div>
+       {/* Income vs Expense */}
+       <div>
+           <h1 className="text-2xl text-chart-4 font-medium p-2 border-b ">Income vs Expense</h1>
+           <div className="bg-card p-2 w-full max-w-xl  h-100 m-auto">
+              <Line  data={DataLine}  options={lineOptions}/>
+           </div>
+       </div>
+    </div>
+  );
 }
 
 export default ReportPage
