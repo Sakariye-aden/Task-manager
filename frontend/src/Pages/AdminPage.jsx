19d5d07cf88a5211e228c41f9839f0c79d0ff395
Sakariye-aden <sakariyeadenhaji@gmail.com>
2026-01-01 16:53:28 +0300

    added an Alert dialog to ensure to delete

--- frontend/src/Pages/AdminPage.jsx
+++ frontend/src/Pages/AdminPage.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ArrowRightLeft, User } from 'lucide-react';
+import { ArrowRightLeft, Loader, Pencil, User } from 'lucide-react';
 import { useQuery } from '@tanstack/react-query';
 
 import api from '../lib/Api/ApiClient'
 
-
-
+import { AlertDialog, AlertDialogCancel,
+  AlertDialogContent, AlertDialogDescription,AlertDialogFooter,
+   AlertDialogHeader,AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+import {Button } from '@/components/ui/button'
 
 
 
 
 
  const AdminPage = () => {
+ 
+      const [ editRole , setEditRole ]=useState(null)
+      const [isOpen , setIsOpen ]= useState(false);
+
+
+    const handleOpen = ()=>{
+      setIsOpen(false)
+      setEditRole(null)
+    }
 
 
 
+
+    //   Admin Summary 
         const { data , error , isLoading } = useQuery({
            queryKey : ['adminKey'],
            queryFn : async () => {
@@ -24,15 +38,33 @@ import api from '../lib/Api/ApiClient'
               return response.data
            }
         })
-
-
+     
+        // user Information
+        const { data:Data  , isLoading: Loading } = useQuery({
+           queryKey : ['adminInfo'],
+           queryFn : async () => {
+              const response = await api.get('/admin/userinfo');
+             
+              return response.data
+           }
+        })
 
    
+         
+
+       
+      if(isLoading || Loading){
+       return (
+          <div className='h-screen flex justify-center items-center'>
+             <Loader className='animate-spin text-3xl' />
+          </div>
+       )
+     }
 
 
-
-
-
+  const handleEdit = (User)=>{
+      setEditRole(User)
+  }
 
 
 
@@ -80,10 +112,96 @@ import api from '../lib/Api/ApiClient'
             </div>
         </div>
       {/* User Management  */}
-
+           <div>
+               <h3 className='text-xl font-medium py-4 my-3 '>User Management</h3>
+               <div className="max-h-96 overflow-y-auto border border-gray-200 rounded-lg shadow-md">
+            <table className="min-w-full border border-gray-200 rounded-lg shadow-md ">
+              <thead className="bg-gray-100 text-gray-700 sticky top-0">
+                <tr>
+                  <th className="px-4 py-2 text-left text-sm font-semibold">
+                   User
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold">
+                    Email
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold">
+                    Role
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold">
+                    Status
+                  </th>
+                  <th className="px-4 py-2 text-left text-sm font-semibold">
+                    Actions
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {Data?.map((User) => (
+                  <tr
+                    key={User._id}
+                    className="hover:bg-chart-1 transition-colors duration-200"
+                  >
+                    <td className="px-4 py-2">
+                       {User.name}
+                    </td>
+                    <td className="px-4 py-2 ">{User.email}</td>
+                    <td className="px-2 py-2 ">{User.role}</td>
+                    <td>
+                      Active
+                    </td>
+                    <td className="px-2 py-2 flex ">
+                        Role
+                      <button
+                        className="p-1  text-sm  rounded cursor-pointer "
+                        onClick={() => handleEdit(User)}
+                      >
+                        <Pencil className="w-4 h-4 text-blue-500" />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+           </div>
       {/* cateGory management */}
 
       {/* Transaction monitoring */}
+
+
+
+        {/* Alert Dialog */}
+         {/* alert Dialog  */}
+              <AlertDialog open={isOpen || !!editRole} onOpenChange={handleOpen}>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>
+                      Are you absolutely sure to change the role ?
+                    </AlertDialogTitle>
+                    <AlertDialogDescription>
+                      <span className="text-lg font-medium pr-1">
+                        {editRole?.name}
+                      </span>
+                       will take more power to see more sensitive data that the other users are not allowed to see please carefully! he has a right to change the other users Role
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+        
+                    <Button >
+                      {/* {DeleteMutation.isPending ? (
+                        <span className="flex justify-center items-center gap-2">
+                          <Loader className="animate-spin" />
+                          Delete
+                        </span>
+                      ) : (
+                        "Delete"
+                      )} */}
+                       Update
+                    </Button>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
     </div>
   )
 }
